d8e67c9fb16584d8aec3cb312c875f9bfe0ebeda
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-09-17 16:49:38 -0600

    [REL] statusbar: Update status bar version string to "v0.2.4"
    The file `internal/tui/components/statusbar/statusbar.go` was modified to update the version string displayed in the status bar from "v0.2.3" to "v0.2.4". This change directly supports the stated purpose of modifying the program version.

--- internal/tui/components/statusbar/statusbar.go
+++ internal/tui/components/statusbar/statusbar.go
@@ -76,7 +76,7 @@ func (sb StatusBar) Render() string {
 	version := sb.theme.AppStyles().Base.
 		Background(sb.theme.Black).
 		Foreground(sb.theme.White).
-		Padding(0, 1).SetString("v0.2.3")
+		Padding(0, 1).SetString("v0.2.4")
 
 	prefixStyle := sb.theme.AppStyles().Base.Padding(0, 2)
 	fillContent := sb.theme.AppStyles().Base
